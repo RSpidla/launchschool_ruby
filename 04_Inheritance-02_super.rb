4e15a6633ea1207451ffdf98ee81fa0bd34528d0
‘RSpidla’ <rspidla@gmail.com>
2019-03-17 16:36:00 +0000

    04_Inheritance-02_super.rb

---
+++ 04_Inheritance-02_super.rb
@@ -0,0 +1,69 @@
+class Animal
+  def speak
+    "Hello!"
+  end
+end
+
+class GoodDog < Animal
+  attr_accessor :name
+
+  def initialize(n)
+    self.name = n
+  end
+
+  def speak
+    "#{self.name} says arf!"
+  end
+end
+
+class Cat < Animal
+end
+
+sparky = GoodDog.new("Sparky")
+paws = Cat.new
+
+puts sparky.speak           # => Sparky says arf!
+puts paws.speak             # => Hello!
+
+# class Animal
+#   def speak
+#     "Hello!"
+#   end
+# end
+
+# class GoodDog < Animal
+#   def speak
+#     super + " from GoodDog class"
+#   end
+# end
+
+# sparky = GoodDog.new
+# sparky.speak        # => "Hello! from GoodDog class"
+
+# good_dog_class.rb
+
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class GoodDog < Animal
+  def initialize(color)
+    super
+    @color = color
+  end
+end
+
+bruno = GoodDog.new("brown")        # => #<GoodDog:0x007fb40b1e6718 @color="brown", @name="brown">
+
+class BadDog < Animal
+  def initialize(age, name)
+    super(name)
+    @age = age
+  end
+end
+
+BadDog.new(2, "bear")        # => #<BadDog:0x007fb40b2beb68 @age=2, @name="bear">
